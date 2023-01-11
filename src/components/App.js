15f92b6ce8d5ba075321b3d9b60e3bb2ecc2ccc2
yuhsuanlii <charlie9684@gmail.com>
2023-01-11 15:34:16 +0800

    test

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ class App extends Component {
     render() {
         return (
             <div>
-                <h1>Hello world webpack hey!</h1>
+                <h1>Hello world webpack hey hello!</h1>
             </div>
         )
     }
